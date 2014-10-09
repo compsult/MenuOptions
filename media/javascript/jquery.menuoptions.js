0f3add2bac412e3f0bbc6c024a2709d431f12136
Mike <compsult@yahoo.com>
2014-10-09 13:38:28 -0400

    prettify (no logic changes)

--- media/javascript/jquery.menuoptions.js
+++ media/javascript/jquery.menuoptions.js
@@ -8,14 +8,14 @@
  * @license         Menu Options jQuery plug-in is licensed under the MIT license
  * @link            http://www.menuoptions.org
  * @docs            http://www.menuoptions.org
- * @version         Version 1.2.5
+ * @version         Version 1.2.4
  *
  ******************************************/
 ;(function ( $, window, document, undefined ) {
 
     var defaults = {
 	   "ClearBtn": false,   // if set, will clear the input field to it's left
-	   "SelectOnly": false,  // if true, will not allow user to type input, only values from the select box will be allowed
+	   "SelectOnly": false,  // if true, will not allow user to type input
 	   "ID": "UnIqDrOpDoWnSeLeCt",
 	   "Data": "",  // pass in your array, object or array of objects here
 	   "ColumnCount" : 1,
@@ -25,7 +25,7 @@
 	   "ShowAt": "bottom",
 	   "Sort": true,
 	   "TriggerEvent": "",
-	   "MenuOptionsType": "Select" // the other option is Navigate, to run JS or follow an href
+	   "MenuOptionsType": "Select" //other option is Navigate (run JS,follow href)
         };
 
     // constructor
@@ -57,20 +57,22 @@
        re_serialize : function ( get_string ) {
             var new_get_str ="";
             $.each(get_string.split('&'), function(k,v) {
-                 $.each(v.split('='), function (k2, v2) {
-                  if ( $('input[name="'+v2+'"]') && $('input[name="'+v2+'"]').attr('key') ) {
-                      new_get_str += v2+"="+$('input[name="'+v2+'"]').attr('key')+"&";  
-                    } else {
+               $.each(v.split('='), function (k2, v2) {
+                  if ( $('input[name="'+v2+'"]') && 
+                       $('input[name="'+v2+'"]').attr('key') ) {
+                      new_get_str += v2+"="
+                          +$('input[name="'+v2+'"]').attr('key')+"&";  
+                  } else {
                       new_get_str += v2+"="+$('input[name="'+v2+'"]').val()+"&"; 
-                    }
+                  }
                     return false;
-                  });
                });
-              return new_get_str.slice(0, -1);
+            });
+            return new_get_str.slice(0, -1);
        },
 
        detect_destroyed_input: function ( base ) {
-           /*--  for Select type, if "parent" element is removed, widget gets removed too  --*/
+           /*--  for Select type, if "parent" elem is removed, widget is too  --*/
            if ( base.options.MenuOptionsType === "Select" ) {
               $(this.element).bind('remove', function(e) {
                  $('span#'+base.options.ID).remove(); 
@@ -83,17 +85,22 @@
            Row=1,
            buffer= "<table class=CrEaTeDtAbLeStYlE cellpadding=4px >";
            base.build_array_of_objs ( base );
-           if (!( base.options.Data instanceof Array ) || base.options.Data[0] instanceof Object ) { // handle single object or an array of objects
-              /*--  this.ary_of_objs.sort(function(a,b) {return (parseInt(a.ky,10) > parseInt(b.ky,10)) ? 1 : ((parseInt(b.ky,10) > parseInt(a.ky,10)) ? -1 : 0);} );  --*/
+           if (!( base.options.Data instanceof Array ) || 
+                  base.options.Data[0] instanceof Object ) { 
+              // handle single object or an array of objects
               if ( base.options.Sort === true ) { 
-                  this.ary_of_objs.sort(function(a,b) {return ((a.val) > (b.val) ? 1 : ((b.val) > (a.val)) ? -1 : 0);} );
+                  this.ary_of_objs.sort(function(a,b) {return ((a.val) > 
+                              (b.val) ? 1 : ((b.val) > (a.val)) ? -1 : 0);} );
               }
               if ( base.options.UseValueForKey === true ) {
                   $.each(this.ary_of_objs, function(k, v) { v.val = v.ky; });
               }
               if ( base.options.ColumnCount === 1 ) {
                   $.each(this.ary_of_objs, function(k, v) { 
-                     buffer+=(( Row % 2 === 0 ) ? "<tr class=even><td class=dflt key='"+v.ky+"'>"+v.val+"</td></tr>" : "<tr class=odd><td class=dflt key='"+v.ky+"'>"+v.val+"</td></tr>");
+                     buffer+=(( Row % 2 === 0 ) ? 
+                             "<tr class=even><td class=dflt key='"+v.ky+"'>"+v.val
+                             +"</td></tr>" : "<tr class=odd><td class=dflt key='"
+                             +v.ky+"'>"+v.val+"</td></tr>");
                      Row += 1;
                   });
               } else {
@@ -101,12 +108,14 @@
                   $.each(this.ary_of_objs, function(k, v) { 
                       TDcnt += 1;
                       buffer += "<td class=dflt key='"+v.ky+"'>"+v.val+"</td>";
-                      if ( TDcnt > 1  && ((TDcnt-1) % base.options.ColumnCount) === 0 ) { 
-                          if ( Row*base.options.ColumnCount < base.total_rec_cnt ) {  
+                      if ( TDcnt > 1  && 
+                              ((TDcnt-1) % base.options.ColumnCount) === 0 ) { 
+                         if ( Row*base.options.ColumnCount < base.total_rec_cnt ) {  
                             Row += 1; 
                             buffer+="</tr>"; 
-                            buffer+=(( Row % 2 === 0 ) ? "<tr class=even>" : "<tr class=odd>"); 
-                          }  
+                            buffer+=(( Row % 2 === 0 ) ? 
+                                    "<tr class=even>" : "<tr class=odd>"); 
+                         }  
                          TDcnt=1; 
                       } 
                   });
@@ -115,9 +124,17 @@
               if ( base.options.ColumnCount === 1 ) {
                  $.each(base.options.Data, function(key, value) {
                      if ( base.options.UseValueForKey === true ) {
-                         buffer+=(( Row % 2 === 0 ) ? "<tr class=even><td class=dflt key='"+value+"'>"+value+"</td></tr>" : "<tr class=odd><td class=dflt key="+value+">"+value+"</td></tr>");
+                         buffer+=(( Row % 2 === 0 ) ? 
+                                 "<tr class=even><td class=dflt key='"+value
+                                 +"'>"+value+"</td></tr>" : 
+                                 "<tr class=odd><td class=dflt key="+value
+                                 +">"+value+"</td></tr>");
                      } else { 
-                         buffer+=(( Row % 2 === 0 ) ? "<tr class=even><td class=dflt key='"+key+"'>"+value+"</td></tr>" : "<tr class=odd><td class=dflt key="+key+">"+value+"</td></tr>");
+                         buffer+=(( Row % 2 === 0 ) ? 
+                                 "<tr class=even><td class=dflt key='"+key
+                                 +"'>"+value+"</td></tr>" : 
+                                 "<tr class=odd><td class=dflt key="+key
+                                 +">"+value+"</td></tr>");
                      }
                      Row += 1;
                  });
@@ -134,7 +151,8 @@
                          if ( Row*base.options.ColumnCount < base.total_rec_cnt ) {
                             Row += 1;
                             buffer+="</tr>";
-                            buffer+=(( Row % 2 === 0 ) ? "<tr class=even>" : "<tr class=odd>");
+                            buffer+=(( Row % 2 === 0 ) ? "<tr class=even>" : 
+                                    "<tr class=odd>");
                          }
                          TDcnt=1;
                       }
@@ -150,7 +168,8 @@
               buffer+="</tr>";
            }
            buffer+="</table>";
-            if ( base.options.MenuOptionsType === "Navigate" ) { // delete all the key props or attr 
+            // delete all the key props or attr 
+            if ( base.options.MenuOptionsType === "Navigate" ) {
                 base.html=buffer.replace(/ key='.*?>/ig, '>') 
             } else { // keep the key props or attr 
                base.html=buffer; 
@@ -158,9 +177,10 @@
     },
 
     add_clear_btn : function ( base ) {
-        /*--  v1.2.4 made clear button simulate mouseover  --*/
         if (base.options.ClearBtn && base.options.MenuOptionsType === "Select") {
-            var ClrBtn = "<div class=clear_btn onclick=\"$(this).siblings('input').val('');$(this).siblings('input').prop('key','');$(this).siblings('input').mouseover()\"></div>";
+            var ClrBtn = "<div class=clear_btn onclick=\"$(this).siblings('input')"
+                +".val('');$(this).siblings('input').prop('key','');"
+                +"$(this).siblings('input').mouseover()\"></div>";
             $(this.element).after(ClrBtn); 
             $('div.clear_btn').on ('mouseenter mouseleave', function() {
                 $(this).toggleClass('ClearButtonMO');
@@ -178,7 +198,8 @@
            }
            base.total_rec_cnt += 1;
         });
-        if ( base.options.MenuOptionsType === "Navigate" ) { // reverse key value pair
+        if ( base.options.MenuOptionsType === "Navigate" ) { 
+           // reverse key value pair
            base.ary_of_objs=$.map(base.ary_of_objs, function (k,v) { 
                      return { ky: k.val, val: k.ky }; 
            });
@@ -240,7 +261,8 @@
                   } else {
                      SelectedCellValue = $(this).html().replace(/^<.*>/, "");
                      MatchedObjects = $.grep(base.ary_of_objs, function(rec){ 
-                                         return SelectedCellValue === rec.val.replace(/^<.*>/, ""); });
+                                         return SelectedCellValue === 
+                                             rec.val.replace(/^<.*>/, ""); });
                      if ( MatchedObjects && MatchedObjects.length > 0 ) {
                         if ( $.isFunction(MatchedObjects[0].ky) ) {
                            MatchedObjects[0].ky.call();
@@ -255,48 +277,75 @@
              $('span#'+base.options.ID).on('mouseleave', function(e) {
                      $('span#'+base.options.ID).remove();  
              });
-             $("span#"+base.options.ID+", span#"+base.options.ID+" > table").css({  zIndex: 9999 });  
+             $("span#"+base.options.ID+", span#"+base.options.ID
+                     +" > table").css({  zIndex: 9999 });  
              base.menu_start_loc = $('span#'+base.options.ID).offset();
-             base.width_adj.width_menu = parseInt($("span#"+base.options.ID+" > table").css('width'),10); 
-             base.width_adj.width_after_adj = (parseInt($("span#"+base.options.ID+" > table").css('width'),10) > $(this).width()) ? parseInt($("span#"+base.options.ID+" > table").css('width'),10) : $(this).width(); 
-             if ( base.options.Width !== "" ) {  // if user specified width, it trumps all
+             base.width_adj.width_menu = parseInt($("span#"+base.options.ID
+                         +" > table").css('width'),10); 
+             base.width_adj.width_after_adj = (parseInt($("span#"
+                            +base.options.ID+" > table").css('width'),10) > 
+                            $(this).width()) ? parseInt($("span#"+base.options.ID
+                            +" > table").css('width'),10) : $(this).width(); 
+             if ( base.options.Width !== "" ) {  
+                 // if user specified width, use that width
                  base.width_adj.width_after_adj = (parseInt(base.options.Width));  
              }  
-             $("span#"+base.options.ID+", span#"+base.options.ID+" > table").css({  "width": base.width_adj.width_after_adj });  
+             $("span#"+base.options.ID+", span#"+base.options.ID+
+                     " > table").css({  "width": base.width_adj.width_after_adj });  
              // show the menu
              $('span#'+base.options.ID) 
                     .stop(true,false)
                     .show();
-             // if the menu width was changed, test to see if it changed it's original offsets
+             // If the menu width was changed, 
+             //    test to see if it changed it's original offsets
              // If it did, re-align menu to parent element
-             if ( base.menu_start_loc.left !== $('span#'+base.options.ID).offset().left || base.menu_start_loc.top !== $('span#'+base.options.ID).offset().top ) {
-                if ( $("span#"+base.options.ID) && $("span#"+base.options.ID)[0] ) {
-                    base.width_adj.width_adjustment = parseInt($("span#"+base.options.ID)[0].style.left) + ( ( base.width_adj.width_after_adj - base.width_adj.width_menu) / 2 );
-                    $('span#'+base.options.ID).css({ "left": base.width_adj.width_adjustment }); 
+             if ( base.menu_start_loc.left !== 
+                     $('span#'+base.options.ID).offset().left || 
+                     base.menu_start_loc.top !== 
+                     $('span#'+base.options.ID).offset().top ) {
+                if ( $("span#"+base.options.ID) && 
+                        $("span#"+base.options.ID)[0] ) {
+                    base.width_adj.width_adjustment = parseInt($("span#"
+                                +base.options.ID)[0].style.left) + 
+                        ( ( base.width_adj.width_after_adj - 
+                            base.width_adj.width_menu) / 2 );
+                    $('span#'+base.options.ID).css({ "left": 
+                        base.width_adj.width_adjustment }); 
                 }
               } 
          });  
          $(this.element).on('mouseleave', function(e) {
-               // the top & bottom adjustments provide overlap between element & drop down||right
-               base.menu_box.top = $('span#'+base.options.ID).position().top - base.menu_box.overlap;
-               base.menu_box.bottom = base.menu_box.top + $('span#'+base.options.ID).height() + base.menu_box.overlap;
-               base.menu_box.left = $('span#'+base.options.ID).position().left - base.menu_box.overlap;
-               base.menu_box.right = base.menu_box.left + $('span#'+base.options.ID).width();
+               // the top & bottom adjustments provide overlap between 
+               // element & drop down||right
+               base.menu_box.top = $('span#'+base.options.ID).position().top 
+                   - base.menu_box.overlap;
+               base.menu_box.bottom = base.menu_box.top 
+                   + $('span#'+base.options.ID).height() + base.menu_box.overlap;
+               base.menu_box.left = $('span#'+base.options.ID).position().left 
+                   - base.menu_box.overlap;
+               base.menu_box.right = base.menu_box.left 
+                   + $('span#'+base.options.ID).width();
                // is the mouse over the drop down? If not, remove it from DOM
-               if ( ! ( e.pageX >= base.menu_box.left && e.pageX <= base.menu_box.right && e.pageY >= base.menu_box.top && e.pageY <= base.menu_box.bottom ) ) {
-                      $('span#'+base.options.ID).remove();  
+               if ( ! ( e.pageX >= base.menu_box.left && 
+                        e.pageX <= base.menu_box.right && 
+                        e.pageY >= base.menu_box.top && 
+                        e.pageY <= base.menu_box.bottom ) ) {
+                            $('span#'+base.options.ID).remove();  
                }
          });
        }
    };
 
     $.fn['menuoptions'] = function ( options ) {
-        if ( options.Utility !== "" && $.isFunction(MenuOptions.prototype[options.Utility]) ) {
-            return MenuOptions.prototype[options.Utility].call(this,options.data);
+        if ( options.Utility !== "" && 
+                $.isFunction(MenuOptions.prototype[options.Utility]) ) {
+            return MenuOptions.prototype[options.Utility]
+                    .call(this,options.data);
         } else {
             return this.each(function () {
                 if (!$.data(this, 'plugin_menuoptions')) {
-                    $.data(this, 'plugin_menuoptions', new MenuOptions( this, options ));
+                    $.data(this, 'plugin_menuoptions', 
+                            new MenuOptions( this, options ));
                 }
             });
         }
